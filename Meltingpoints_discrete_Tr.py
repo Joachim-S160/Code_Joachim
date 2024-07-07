4cd9e47418fb9bbc3d7cd1bdececea136cab3a77
joaschee <Joachim.Scheerlinck@UGent.be>
2024-07-07 23:28:19 +0200

    Discrete_method_1.0

---
+++ Meltingpoints_discrete_Tr.py
@@ -0,0 +1,122 @@
+"""
+
+Description:
+Halide melting point prediction using CHGNet
+using the dicrete temperature density method
+1 NPT Equilibration at different temerpatures
+no more than 500 atoms in the box
+
+"""
+
+from __future__ import annotations
+from chgnet.model import MolecularDynamics
+from chgnet.model import StructOptimizer
+from pymatgen.core import Structure
+from chgnet.model import CHGNet
+
+# get biggest box with less than 500 atoms
+
+
+def Biggest_box(structure):
+    """
+    Args: structure
+    Returns: dimensions of the biggest possible supercell box    
+    """
+    # Get number of atoms in unitcell
+    noau = structure.num_sites
+    print(noau)
+    # Get the boxdimension
+    Boxdim = int((500/noau)**(1/3))
+    a, b, c = Boxdim, Boxdim, Boxdim
+
+    noa = (a+1)*b*c*noau
+    # Update boxdimensions until noa is bigger than 500 or the boxdimensions are the same as before
+    while noa < 500:
+        x, y, z = a, b, c
+        if (a+1)*b*c*noau < 500:
+            a += 1
+
+        if a*(b+1)*c*noau < 500:
+            b += 1
+
+        if a*b*(c+1)*noau < 500:
+            c += 1
+        if [x, y, z] == [a, b, c]:
+            break
+    print("Total number of atoms in the box: ", a*b*c*noau)
+    return [a, b, c]
+
+# first relax the structure
+# NVT for 20 ps
+# than NPT for 100 ps
+# only for every 100 K
+
+
+def Melting_point_simulation(molecule_name, cif_file, Tstart=100, Tend=1000, GPU="cuda:2"):
+    """
+    Args:   molecule_name: name of the molecule
+            cif_file: cif file of the molecule
+            Tstart: start temperature
+            Tend: end temperature
+            GPU: GPU to use
+    Returns: None, stores trajectory and log file
+    """
+
+    # load structure
+    structure = Structure.from_file(cif_file)
+    print(f"structure: {structure}")
+
+    # load model
+    chgnet = CHGNet.load()
+
+    # Relax the structure so that the atoms are moved to positions with lower potential energy and the cell size is adjusted to the optimal size with no stresses.
+    relaxer = StructOptimizer()
+    relaxed_structure_dict: dict = relaxer.relax(structure, verbose=True)
+    print(
+        f"\nCHGNet took {len(relaxed_structure_dict['trajectory'])} steps. Relaxed structure:")
+    print(relaxed_structure_dict["final_structure"])
+    relaxed_structure: Structure = relaxed_structure_dict["final_structure"]
+
+    # Check if relaxed structure is a structure type
+    assert isinstance(relaxed_structure,
+                      Structure), "Relaxed structure is not a structure type"
+
+    # Molecular dynamics simulations
+
+    # Make a axbxc supercell structure which is a*b*c copies of the original structure (noau atoms) = a*b*c*noau atoms
+    relaxed_structure.make_supercell(Biggest_box(relaxed_structure))
+
+    print('Presimulation code works, start md1')
+    for temp in range(Tstart, Tend, 100):
+        print(temp)
+        md1 = MolecularDynamics(
+            atoms=relaxed_structure,
+            model=chgnet,
+            ensemble="nvt",
+            temperature=temp,  # in K
+            timestep=2,  # in fs, taut=500*1000000
+            trajectory="mdNVT_out_" + molecule_name + \
+            "_" + str(temp) + ".traj",
+            logfile="mdNVT_out_" + molecule_name + "_" + str(temp) + ".log",
+            loginterval=100,
+            use_device=GPU,  # use 'cuda' for faster MD
+        )
+        md1.run(500*20)  # run 20 ps
+
+        md2 = MolecularDynamics(
+            atoms=md1.atoms,
+            model=chgnet,
+            ensemble="npt",
+            temperature=temp,  # in K
+            timestep=2,  # in fs, taut=500*1000000
+            trajectory="mdNPT_out_" + molecule_name + \
+            "_" + str(temp) + ".traj",
+            logfile="mdNPT_out_" + molecule_name + "_" + str(temp) + ".log",
+            loginterval=100,
+            use_device=GPU,  # use 'cuda' for faster MD
+        )
+        md2.run(500*100)  # run 100 ps
+        print('md ' + str(temp) + ' done')
+
+
+Melting_point_simulation("Al", "Al.cif", Tstart=400, Tend=1400, GPU="cuda:2")
---
+++ get_data.py
@@ -0,0 +1,143 @@
+"""
+
+Description:
+Get data from trajectory files
+and return physical properties
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from chgnet.model import CHGNet
+from ase.io.trajectory import Trajectory
+from pymatgen.io.ase import AseAtomsAdaptor
+
+
+
+def normalize(x: np.ndarray):
+    """
+    Args: array x
+    returns: normalized array x    
+    """
+    return (x-np.min(x))/(np.max(x)-np.min(x))
+
+def get_temperature(file: str):
+    """
+    input = trajectory file
+    output = 1D array of temperature in Kelvin
+    """
+    traj = Trajectory(file)[:]
+    return np.array([atoms.get_temperature() for atoms in traj])
+
+def get_number_of_atoms(file:str):
+    """
+    input = trajectory file
+    output = 1D array of number of atoms
+    """
+    traj = Trajectory(file)[:]
+    return np.array([len(atoms) for atoms in traj])
+
+def get_time(file: str):
+    """
+    input = trajectory file
+    output = 1D array of time in picoseconds
+    """
+    traj = Trajectory(file)[:]
+    # * 0.2 to convert from fs to ps (timesep is 2 fs and trajectory saves data every loginterval = 100 steps) so * 2 * 100 /1000 to get ps
+    return np.arange(len(traj)) * 0.2
+
+def get_volume(file: str):
+    """
+    input = trajectory file
+    output = 1D array of volume in A^3
+    """
+    traj = Trajectory(file)[:]
+    return np.array([atoms.get_volume() for atoms in traj])
+
+def get_density(file: str):
+    """
+    input = trajectory file
+    output = 1D array of density in g/cm^3
+    """
+    traj = Trajectory(file)[:]
+    Volume = get_volume(file)
+    mass = traj[0].get_masses().sum()
+    # Convert density from amu/A^3 to g/cm^3 by multiplying by 1.66054
+    return 1.66054 * mass / Volume
+
+def get_pressure(file: str):
+    """
+    input = trajectory file,
+    output = 1D array of pressure in GPa
+    """
+    # stress tensors = sigma_ij at each frame
+    # sigma_ij = sigma_xx, sigma_yy, sigma_zz, sigma_xy, sigma_xz, sigma_yz
+    # pressure = - 1/3 * (sigma_xx + sigma_yy + sigma_zz)
+    traj = Trajectory(file)[:]
+    stresstensors = [atoms.get_stress(include_ideal_gas=True) for atoms in traj]
+    pressure = np.array([-1/3 * (sigma[0] + sigma[1] + sigma[2]) for sigma in stresstensors]) 
+    # Conversion from eV/A^3 to GPa:
+    pressure = pressure * 160.21766208
+    return pressure
+
+def get_total_energy(file: str):
+    """
+    input: trajectory file
+    output: 1D array of total energy in eV
+    """
+    traj = Trajectory(file)[:]
+    return np.array([atoms.get_total_energy() for atoms in traj])
+
+def get_enthalpy(file: str, one_atm_pressure:bool=True):
+    """
+    input = trajectory file,
+            one_atm_pressure = if True pressure is 1 atm, if False the real variable pressure is used 
+    output = 1D array of enthalpy in eV
+    """
+    if one_atm_pressure:
+        # Convert 1 atm to eV/A^3: atm to GPA is 10**(-4) and GPa to eV/A^3 is 1/160
+        pressure = 1/160 * 10**(-4) * np.ones_like(get_pressure(file))
+    else:
+        pressure = get_pressure(file)
+    Etot = get_total_energy(file)
+    Volume = get_volume(file)
+    return normalize(Etot + pressure * Volume)
+
+def get_kinetic_energy(file: str):
+    """
+    input = trajectory file
+    output = 1D array of kinetic energy in eV
+    """
+    return np.array([atoms.get_kinetic_energy() for atoms in Trajectory(file)[:]])
+
+def get_potential_energy(file: str):
+    """
+    input = trajectory file
+    output = 1D array of potential energy in eV
+    """
+    return np.array([atoms.get_potential_energy() for atoms in Trajectory(file)[:]])
+
+def time_averaging(quantity: np.ndarray, window_factor: float = 0.5, stdv: bool = False):
+    """
+    Args:   
+            1D array of quantity to be averaged
+            window_factor = factor to determine the size of the window
+    returns:
+            averaged quantity 
+    """
+    assert window_factor <= 1, "window_factor must be less than 1"
+    if stdv:
+        return np.std(quantity[int(np.round(len(quantity)*window_factor)):])
+    return np.average(quantity[int(np.round(len(quantity)*window_factor)):])
+
+def convergence_error(quantity: np.ndarray, window_factor: float = 0.5):
+    """
+    Args:   
+            1D array of quantity to be averaged
+            window_factor = factor to determine the size of the window
+    returns:
+            convergence error of the quantity: (min and max)/2 in the window
+    """
+    assert window_factor <= 1, "window_factor must be less than 1"
+    window = quantity[int(np.round(len(quantity)*window_factor)):]
+    return (np.max(window) - np.min(window))/2
